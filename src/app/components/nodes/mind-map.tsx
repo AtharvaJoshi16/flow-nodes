19588544e9d6cf9695681fd307e604d45047cb15
Atharva Joshi <atharvajoshi@Atharvas-MacBook-Pro.local>
2024-03-04 01:03:35 +0530

    added color pallete and bold italic features

--- src/app/components/nodes/mind-map.tsx
+++ src/app/components/nodes/mind-map.tsx
@@ -1,15 +1,22 @@
 import { Alignment } from "@/app/(slice)/optionsSlice";
 import { RootState } from "@/app/(store)";
 import { Input } from "@/components/ui/input";
-import { MouseEvent, useEffect, useRef, useState } from "react";
+import { CSSProperties, MouseEvent, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Handle, NodeProps, NodeToolbar, Position } from "reactflow";
-
+import { TwitterPicker } from "react-color";
 import { FontBoldIcon, FontItalicIcon } from "@radix-ui/react-icons";
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { Trash2 } from "lucide-react";
+import { Palette, Trash2 } from "lucide-react";
 import { updateNodeStyle } from "@/app/(slice)/nodeSlice";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useTheme } from "next-themes";
 export const MindMapNode = ({
   node,
   data,
@@ -20,45 +27,45 @@ export const MindMapNode = ({
   setNodeName,
 }) => {
   const alignment = useSelector((state: RootState) => state.options.alignment);
-  const nodes = useSelector((state: RootState) => state.nodes.nodes);
-
+  const { theme } = useTheme();
+  const [styling, setStyling] = useState({
+    bold: false,
+    italic: false,
+    color: "",
+  });
   const nodeRef = useRef<HTMLDivElement>(null);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    const s = { ...styling };
+    theme === "dark" ? (s.color = "white") : (s.color = "black");
+    setStyling(s);
+  }, [theme]);
+
+  useEffect(() => {
+    const styles: CSSProperties = {
+      fontWeight: styling.bold ? 700 : 500,
+      fontStyle: styling.italic ? "italic" : "normal",
+      color: styling.color,
+    };
+    console.log(styling, styles);
+    dispatch(updateNodeStyle({ id: node.id, style: styles }));
+  }, [styling]);
+
   const handleOption = (e, opt: string) => {
     const state = e.currentTarget.dataset.state;
+    const s = { ...styling };
     switch (opt) {
       case "bold": {
-        dispatch(
-          updateNodeStyle({
-            id: node.id,
-            style:
-              state === "off"
-                ? {
-                    fontWeight: "bold",
-                  }
-                : {
-                    fontWeight: 500,
-                  },
-          })
-        );
+        s.bold = state === "off" ? true : false;
+        setStyling(s);
+        break;
       }
+
       case "italic": {
-        dispatch(
-          updateNodeStyle({
-            id: node.id,
-            style:
-              state === "off"
-                ? {
-                    fontStyle: "italic",
-                  }
-                : {
-                    fontStyle: "normal",
-                  },
-          })
-        );
-      }
-      default: {
+        s.italic = state === "off" ? true : false;
+        setStyling(s);
+        break;
       }
     }
   };
@@ -80,11 +87,15 @@ export const MindMapNode = ({
     >
       <NodeToolbar
         isVisible={data.toolbarVisible}
-        className="z-400"
+        className="z-400 relative"
         position={Position.Top}
         style={{ top: "-10px" }}
       >
-        <ToggleGroup className="bg-slate-200 rounded-md p-1" type="multiple">
+        <ToggleGroup
+          variant="outline"
+          className="border-2 border-slate-500 rounded-md p-1"
+          type="multiple"
+        >
           <ToggleGroupItem
             onClick={(e) => handleOption(e, "bold")}
             value="bold"
@@ -97,9 +108,30 @@ export const MindMapNode = ({
           >
             <FontItalicIcon strokeWidth={3} />
           </ToggleGroupItem>
+          <Popover>
+            <PopoverTrigger>
+              <Button size="icon" variant="outline">
+                <div
+                  style={{ backgroundColor: styling.color }}
+                  className="rounded-full w-[20px] h-[20px]"
+                />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent side="top" className="w-[320px]">
+              <TwitterPicker
+                triangle="hide"
+                color={styling.color}
+                onChangeComplete={(color) => {
+                  const s = { ...styling };
+                  s.color = color.hex;
+                  setStyling(s);
+                }}
+                className="m-auto shadow-none"
+              />
+            </PopoverContent>
+          </Popover>
         </ToggleGroup>
       </NodeToolbar>
-
       <Handle
         type="target"
         position={
